0d73a1f8f86cfbfdca85a07e113f18a05efe7045
Mr. Hyde <github@2bad.me>
2016-07-18 20:41:15 +0200

    [js] use custom encoding for each database instance and save result to json file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import Database from './lib/database'
 
-let de = new Database(1, 'de', 'Deutsch')
-let en = new Database(2, 'en', 'Englisch')
-let fr = new Database(3, 'fr', 'Franzoesisch')
-let ru = new Database(4, 'ru', 'Russisch')
+let de = new Database(1, 'de', 'Deutsch', 'ISO-8859-1')
+let en = new Database(2, 'en', 'Englisch', 'ISO-8859-1')
+let fr = new Database(3, 'fr', 'Franzoesisch', 'ISO-8859-1')
+let ru = new Database(4, 'ru', 'Russisch', 'windows-1251')
 
 async function init () {
+  await de.parse()
+  await en.parse()
+  await fr.parse()
   await ru.parse()
 }
 

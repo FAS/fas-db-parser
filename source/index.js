e09e41cde28f99c016a58a082fa33628f4e1ed0a
Mr. Hyde <mr.hyde@2bad.me>
2017-02-25 20:11:53 +0300

    [js] decode page data as `utf-8`

--- source/index.js
+++ source/index.js
@@ -1,9 +1,9 @@
 import Database from './database'
 
-let de = new Database(1, 'de', 'Deutsch', 'ISO-8859-1')
-let en = new Database(2, 'en', 'Englisch', 'ISO-8859-1')
-let fr = new Database(3, 'fr', 'Franzoesisch', 'ISO-8859-1')
-let ru = new Database(4, 'ru', 'Russisch', 'windows-1251')
+let de = new Database(1, 'de', 'Deutsch', 'utf-8')
+let en = new Database(2, 'en', 'Englisch', 'utf-8')
+let fr = new Database(3, 'fr', 'Franzoesisch', 'utf-8')
+let ru = new Database(4, 'ru', 'Russisch', 'utf-8')
 
 async function init () {
   await de.parse()

f6d0fce8dc25cecfb4bc60246d4f6da67ef4549a
Mr. Hyde <mr.hyde@2bad.me>
2017-03-16 02:07:05 +0300

    [js] replace entry point with simple Singleton object

--- source/index.js
+++ source/index.js
@@ -1,15 +1,10 @@
 import Database from './database'
 
-let de = new Database(1, 'de', 'Deutsch')
-let en = new Database(2, 'en', 'Englisch')
-let fr = new Database(3, 'fr', 'Franzoesisch')
-let ru = new Database(4, 'ru', 'Russisch')
-
-async function init () {
-  await de.parse()
-  await en.parse()
-  await fr.parse()
-  await ru.parse()
+const Kleptomaniac = {
+  init: async function (options) {
+    const db = new Database(options.locale)
+    await db.parse()
+  }
 }
 
-init()
+export default Kleptomaniac

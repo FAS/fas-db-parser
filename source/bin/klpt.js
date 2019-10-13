8a696e7d488c00ee4025451554aa11847549b5e0
ArmorDarks <git@lavrins.com>
2019-10-14 00:33:45 +0300

    [scripts] ensure commander passes on numeric options as `number`

--- source/bin/klpt.js
+++ source/bin/klpt.js
@@ -7,9 +7,9 @@ const Kleptomaniac = require('../index.js')
 program
   .usage('')
   .description('FAS DB parsing tool')
-  .option('-s, --start <start>', 'id of the start entry', 0)
-  .option('-e, --end <end>', 'id of the end entry', 1000000)
-  .option('-p, --path <path>', 'path to storage folder', 'db')
+  .option('-s, --start <start>', 'id of the start entry', parseFloat, 0)
+  .option('-e, --end <end>', 'id of the end entry', parseFloat, 1000000)
+  .option('-p, --path <path>', 'path to the storage folder', 'db')
   // @todo Not implemented
   // .option('-i, --input <file>', 'use locale db file')
   // .option('-o, --output <file>', 'destination for db file')
